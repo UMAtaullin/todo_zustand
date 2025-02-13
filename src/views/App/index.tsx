a7ffd46aed861b192c334e5fa45a3d971621f2db
UMAtaullin <ataullinural@gmail.com>
2025-02-13 16:05:36 +0500

    styles: article and Title

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,7 +1,12 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
+import styles from './index.module.scss';
 
 export const App: FC = () => {
   return (
-    <article>App</article>
+    <article className={styles.article}>
+      <h1 className={styles.articleTitle}>To Do App</h1>
+      <section className={styles.articleSection}></section>
+      <section className={styles.articleSection}></section>
+    </article>
   )
 }
